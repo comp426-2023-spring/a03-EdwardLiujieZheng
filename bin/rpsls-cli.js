b48910e280c8dcd48ca9f01af5f0ff18ed1fb38c
Edward Liujie Zheng <liujiez@email.unc.edu>
2023-05-05 12:46:59 -0400

    update rules

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -58,16 +58,16 @@ let result = rpsls(args._[0]);
 if(result == undefined) {
   console.log("Please try 'rock', 'paper', 'scissors', 'lizard', or 'spock'.");
   console.log(`Usage: node-rpsls [SHOT]
-    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-    
-      -h, --help        display this help message and exit
-      -r, --rules       display the rules and exit
-    
-    Examples:
-      node-rpsls        Return JSON with single player RPSLS result.
-                        e.g. {"player":"rock"}
-      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                        e.g {"player":"rock","opponent":"Spock","result":"lose"}
+  Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+  
+    -h, --help        display this help message and exit
+    -r, --rules       display the rules and exit
+  
+  Examples:
+    node-rpsls        Return JSON with single player RPSLS result.
+                      e.g. {"player":"rock"}
+    node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"Spock","result":"lose"}
     `);
     console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
 
